24ed16920f4c892e3861de9f846cb5d00fd9345d
Dheeraj-ram <kdheerajram07@gmail.com>
2023-06-22 10:34:52 +0530

    added an condition to check 200 error

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.get("/numbers", async (req, res) => {
     const all_urls = req.query.urls.split(",");
     const numbers = [];
     for (const i of all_urls) {
-        const response = await fetch(url, { timeout: time });  
+        const response = await fetch(url, { timeout: time });
+        if (response.status === 200) {
+          const number = await response.json();
+          numbers.push(...new Set(number.numbers));
+        }  
     } 
     res.json({ numbers });
   });
