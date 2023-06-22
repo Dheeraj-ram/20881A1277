ec0374c8ce5459f4b4b5826552a57f52f4ac0b5a
Dheeraj-ram <kdheerajram07@gmail.com>
2023-06-22 10:33:50 +0530

    added the code part to fetch the numbers from url

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const app = express();
 const port = 8008;
 const time = 500;
 
+app.get("/numbers", async (req, res) => {
+    const all_urls = req.query.urls.split(",");
+    const numbers = [];
+    for (const i of all_urls) {
+        const response = await fetch(url, { timeout: time });  
+    } 
+    res.json({ numbers });
+  });
+  
 
